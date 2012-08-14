b8b6eaaa003903d851b7f354ed885205f24cb513
司徒正美 <cheng19840218@gmail.com>
2012-08-14 04:09:38 -0700

    更新mongo的文档！

--- test.js
+++ test.js
@@ -3,28 +3,17 @@ var db = new mongo.Db("northwind", new mongo.Server('localhost', 27017, {}), {})
 
 db.open(function() {
     db.collection("posts", function(err, posts) {
-        posts.drop(function(){
-            console.log("xxxxxxxxxx")
-        })
-//        db.posts.count({},function(e,a){
-//            console.log(a)
-//        })
-    })
-//    db.collection("posts", function(err, posts) {
-//        posts.insert([{
-//            aaa:1
-//        },{
-//            bbb:2
-//        },{
-//            ccc:3
-//        }],function(){
-//            posts.find({}, function(e, cursor){
-//                cursor.nextObject(function(){
-//                    console.log(cursor.items)
-//                })
-//            })
-//        })
-     
 
-//  });
+        posts.find().toArray(function(e, cursor){
+            cursor.forEach(function(el){
+                console.log(el._id.generationTime)
+                console.log("created at " + new Date(el._id.generationTime) + "\n")
+            })
+
+
+
+        })
+       
+    })
+
 });
