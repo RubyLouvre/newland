86dc54d9c181ab7cf157cbaeba80635396874869
司徒正美 <cheng19840218@gmail.com>
2012-08-15 04:19:21 -0700

    update test!

--- test.js
+++ test.js
@@ -1,19 +1,20 @@
 var mongo  = require('mongodb');
 var db = new mongo.Db("northwind", new mongo.Server('localhost', 27017, {}), {});
-
 db.open(function() {
-    db.collection("posts", function(err, posts) {
-
-        posts.find().toArray(function(e, cursor){
-            cursor.forEach(function(el){
-                console.log(el._id.generationTime)
-                console.log("created at " + new Date(el._id.generationTime) + "\n")
-            })
-
-
-
+    db.collection("users",function(err, users){
+        users.update({
+            array1:{$exists : true }
+        },{//移除第一个
+            $rename:{
+                array1: "array4"
+            }
+        },true,function(err){
+            users.find().toArray(function(e, els){
+                els.forEach(function(el){
+                    console.log(el)
+                });
+            });
         })
-       
+    
     })
-
-});
+})
