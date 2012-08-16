f7d7f45648dd8299edd7f80a163d48cfd19da2d2
RubyLouvre <cheng19840218@gmail.com>
2012-08-16 08:04:34 +0800

    更新mongo的update文档

--- test.js
+++ test.js
@@ -2,10 +2,19 @@ var mongodb = require('mongodb'),
 server = new mongodb.Server("127.0.0.1", 27017, {});
 new mongodb.Db('test', server, {}).open(function (error, db) {
     db.collection("users", function(err, users){
-       users.save({user:"David"},{safe:true}, function(){
-           users.find().toArray(function(e, items){
-               console.log(items)
-           })
-       })
+        //当update是替换操作时,即第个参数没有操作符,那么第三个可选对象即使集团了multi为true,它也每次替换一个
+        users.findAndModify({
+            safe:true
+        },[],{
+            $unset:{safe:1}
+        },
+        {
+            safe:true
+        }, function(err, ee){
+            console.log(ee)
+            users.find().toArray(function(e, items){
+                console.log(items)
+            })
+        })
     })
 });
