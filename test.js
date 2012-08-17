ca31d219098153311ac94b629c243d0e9ec4efa0
司徒正美 <cheng19840218@gmail.com>
2012-08-17 02:43:14 -0700

    更新芒果的update页面

--- test.js
+++ test.js
@@ -1,10 +1,25 @@
 var mongodb = require('mongodb'),
 server = new mongodb.Server("127.0.0.1", 27017, {});
+var life = 60* 60 * 24;
 new mongodb.Db('test', server, {}).open(function (e, db) {
-  db.removeUser("aaa",{safe: true}, function(e,ok){
-      console.log(e);
-      console.log(ok)//true
-  })
-});
+    db.collection("dogs", function(e,table){
+        table.findAndModify ({
+            sid: "rubylouvre",
+            life: life
+        },[],{
+            $set:{
+                timestamp: new Date * 1 * life
+            }
+        },{
+            safe:true,
+            "new":true,
+            upsert: true
+        }, function(e, a){
+            console.log(e)
+            console.log(a)
+        })
+    })
+})
+
 
 
