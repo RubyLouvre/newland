79a2b57b136d7885372384187217bbb6a67c7d91
RubyLouvre <cheng19840218@gmail.com>
2012-08-19 21:24:35 +0800

    添加限制条件

--- system/services/session.js
+++ system/services/session.js
@@ -1,5 +1,8 @@
 $.define("session","../sessions/"+$.config.session.type ,function(fn){
     return function(flow){
-        fn(flow)
+        if(flow.cookies){
+            fn(flow)
+        }
     }
 })
+//202.8.19 此服务受限于get_cookie服务
