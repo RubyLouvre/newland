b344c7f0d6900de76739b95d609ce3317e5083e4
RubyLouvre <cheng19840218@gmail.com>
2012-08-19 21:23:16 +0800

    重构get_cookie服务，只有经过MVC的请求才能进入，同时也可减少session服务的无谓消防，提高性能

--- system/services/get_cookie.js
+++ system/services/get_cookie.js
@@ -1,15 +1,10 @@
 $.define("get_cookie","../cookie", function(Cookie){
     return function( flow ){
         var config = $.config.cookie
-        if ( flow.originalUrl.indexOf(config.path) == 0 ){
+        if ( flow.mime == "*" && flow.originalUrl.indexOf(config.path) == 0 ){
             var cookie = flow.req.headers.cookie;
-            if (!cookie) {
-                flow.cookies = {};
-            }else{
-                flow.cookies = Cookie.parse(cookie)
-            }
-            flow.fire("get_cookie",  Cookie );
+            flow.cookies = cookie ?  Cookie.parse(cookie) : {}
         }
-     
     }
 });
+// 2012.8.19 只有经过MVC的请求才能进入，同时也可减少session服务的无谓消防，提高性能
