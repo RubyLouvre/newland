255d4b86ce4863941b34505408fdb48a0f476f1c
RubyLouvre <cheng19840218@gmail.com>
2012-08-19 13:58:00 +0800

    为模块改名，让其更短

--- system/sessions/cookie.js
+++ system/sessions/cookie.js
@@ -1,17 +1,15 @@
-$.define("cookie_session", function(){
+$.define("cookie", function(){
     //从cookie中取得加密了的sessionID的值,然后去掉过期时间,转换成一个对象
     //https://github.com/caolan/cookie-sessions/blob/master/lib/cookie-sessions.js
     //缺点是,可以储存的东西太少了
     return function( flow ){
-        flow.bind("get_cookie", function( Cookie ){
+        flow.bind("get_cookie", function( ){
             var s = $.config.session
             var sval = flow.cookies[ s.sid ]
-            var data = sval ? Cookie.deserialize( s.secret, s.life, sval ) : {}
+            var data = sval ? JSON.parse(sval) : {}
             flow.session.open( s.life, data )
             flow.bind("header", function(){
-                flow.res.setHeader("Set-Cookie",  Cookie.serialize(s.sid, flow.session,{
-                    secret: s.secret
-                }))
+                flow.addCookie( s.sid, JSON.stringify(flow.session.data) )
             })
         })
     }
--- system/sessions/memory.js
+++ system/sessions/memory.js
@@ -1,4 +1,4 @@
-$.define("memory_session", function(){
+$.define("memory", function(){
     //它要在app上创建一个hash,
     var memory = $.memory ||  ($.memory = {});
     function sweep (){//清理过期的session
@@ -20,9 +20,10 @@ $.define("memory_session", function(){
         flow.bind("get_cookie", function( ){
             var s = $.config.session
             var sid = flow.cookies[ s.sid ];
-            var data = sid ? memory[ sid ] : memory[ flow.uuid() ]
+            var nid = flow.uuid();
+            var data = sid ? memory[ sid ] : memory[ nid ]
             if( !sid ){
-                flow.res.setHeader("Set-Cookie", sid+"="+flow.uuid)
+                flow.addCookie( sid, nid )
             }
             flow.session.open( s.life, data );
         })
