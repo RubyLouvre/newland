10b2277abf8bcb085fe0b905d779a0d9818c9f9a
RubyLouvre <cheng19840218@gmail.com>
2012-08-20 01:17:14 +0800

    重写memory session服用

--- system/sessions/memory.js
+++ system/sessions/memory.js
@@ -3,9 +3,9 @@ $.define("memory", function(){
     var memory = $.memory ||  ($.memory = {});
     function sweep (){//清理过期的session
         var now = +new Date;
-        for (var key in memory) {
-            if (typeof memory[key] == "object" && memory[key].timestamp < now){
-                delete memory[key]
+        for (var sid in memory) {
+            if ( memory[sid] && !memory[sid].flow && memory[sid].mtime < now){
+                delete memory[sid]
             }
         }
     }
@@ -15,17 +15,17 @@ $.define("memory", function(){
         // 每 24 分钟执行一次
         setInterval(sweep, 1440 * 1000);
     }
-
     return function( flow ){
-            var s = $.config.session
-            var sid = flow.cookies[ s.sid ];
-            var nid = flow.uuid();
-            var data = sid ? memory[ sid ] : memory[ nid ]
-            if( !sid ){
-                flow.addCookie( sid, nid )
-            }
-            console.log("CCCCCCCCCCCCCCC")
-            flow.session.open( s.life, data );
+        var s = $.config.session, data = {};
+        var sid = flow.cookies[ s.sid ];
+        //如果数据还保存在内存中,把它取下来,防止被请掉
+        if( sid && memory[ sid ] ){
+            data = memory.data
+            delete memory[ sid ]
+        }else{
+            sid = flow.uuid();
+            flow.addCookie( s.sid, sid )
+        }
+        flow.store.open( sid, s.life, data );
     }
 });
-
