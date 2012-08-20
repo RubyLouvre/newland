98630d587223ea6ba9059fcf9f17d88408561468
司徒正美 <cheng19840218@gmail.com>
2012-08-19 22:12:03 -0700

    去掉过多的日志打印

--- system/page_generate.js
+++ system/page_generate.js
@@ -1,11 +1,10 @@
 $.define("page_generate","helper, mass/more/ejs, hfs",function(get_hepler){
-   $.log("生成页面")
+    $.log("生成页面")
     var url = $.path.join( process.cwd(),"app/views/").replace(/\\/g,"/"),layouts = {};
     $.walk(url, function(files){
         var pending = files.length;
         for(var i = 0; i < pending; i++){
             (function(view_url){
-
                 $.readFile(view_url,"utf-8", function(e, source){
                     var array = get_hepler();
                     var data = array[0]
@@ -25,7 +24,6 @@ $.define("page_generate","helper, mass/more/ejs, hfs",function(get_hepler){
                                 $.log('<code style="color:red;">找不到必需的布局模板: ', layout_url, '</code>', true);
                             }
                         }
-                      
                         fn = $.ejs.compile(html,array[1]);
                         html = fn(data);
                     }
@@ -33,14 +31,14 @@ $.define("page_generate","helper, mass/more/ejs, hfs",function(get_hepler){
                         var page_url = view_url.replace("/views","/pages");
                         if(page_url !== view_url){
                             $.updateFile(page_url, html, function(){
-                                $.log(page_url+"  同步完成")
-                            },1);
+                                //    $.log(page_url+"  同步完成")
+                                },1);
                         }
                         //同步到rubylouvre项目
                         var rubylouvre = view_url.replace("/app/views","").replace("newland","rubylouvre")
                         $.updateFile(rubylouvre, html, function(){
-                            $.log(rubylouvre+"  同步完成");
-                        },1);
+                            //   $.log(rubylouvre+"  同步完成");
+                            },1);
                     }
                 });
             })( files[i].replace(/\\/g,"/") );//要处理路径时必须先统一path.sep,因为你不知它是/,还是\
