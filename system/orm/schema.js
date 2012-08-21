87e97df19a6bc0b2c72f5f9d2c3c38c276c2158c
RubyLouvre <cheng19840218@gmail.com>
2012-08-21 08:42:05 +0800

    添加ORM系统

---
+++ system/orm/schema.js
@@ -0,0 +1,9 @@
+$.define("schema","../mass/flow", function(){
+    var Schema = $.factory({
+        init: function(){
+
+        },
+        inherit: $.Flow
+    })
+})
+//https://github.com/1602/jugglingdb/blob/master/lib/schema.js
