8e3731f6a9ed29e047fcbccf42a07920eca7be07
RubyLouvre <cheng19840218@gmail.com>
2012-08-22 08:52:49 +0800

    update comments

--- system/more/logger.js
+++ system/more/logger.js
@@ -47,6 +47,33 @@ $.define("logger", "../hfs,../../app/config",function(){
     return Log;
 
 });
+// 最重要的参考 https://github.com/jaekwon/nogg/tree/master/lib
 //https://github.com/joyent/node/wiki/modules#wiki-logs
 // https://github.com/LearnBoost/cluster/blob/master/lib/plugins/logger.js
 //  https://github.com/Gagle/Node-BufferedWriter
+
+//log: function() {
+//		var trace = getTrace(__stack[1]);
+//		var string = util.format("%s [log] in %s:%d \n%s", trace.timestamp, trace.file, trace.lineno, util.format.apply(this, arguments));
+//
+//		process.stdout.write(string + "\n");
+//	},
+//function getTrace(call) {
+//	return {
+//		file: call.getFileName(),
+//		lineno: call.getLineNumber(),
+//		timestamp: new Date().toUTCString()
+//	}
+//	<ul>
+/*
+<li>0 <strong>EMERGENCY</strong>  system is unusable</li>
+<li>1 <strong>ALERT</strong> action must be taken immediately</li>
+<li>2 <strong>CRITICAL</strong> the system is in critical condition</li>
+<li>3 <strong>ERROR</strong> error condition</li>
+<li>4 <strong>WARNING</strong> warning condition</li>
+<li>5 <strong>NOTICE</strong> a normal but significant condition</li>
+<li>6 <strong>INFO</strong> a purely informational message</li>
+<li>7 <strong>DEBUG</strong> messages to debug an application</li>
+</ul>
+*/
+//}
