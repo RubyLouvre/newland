0db2a2465f5eec511b76f933a866dd46399c00cf
司徒正美 <cheng19840218@gmail.com>
2012-08-23 03:42:34 -0700

    update!

--- system/store/test.js
+++
@@ -1,6 +0,0 @@
-require("./mass")
-console.log("xxxxxxxxxxxxx")
-$.require("./queue",function(queue){
-   var a = new queue;
-   console.log(a.length)
-})
--- test.js
+++ test.js
@@ -1,18 +1,9 @@
-var nStore = require("./system/store/nStore").nStore;
 
-
-new nStore('./system/store/data/users.db', function (err, users) {
-    // It's loaded now
-    // Insert a new document with key "creationix"
-    users.save("creationix", {
-        name:"xxxx"
-    }, function (err) {
-        if (err) {
-            throw err;
-        }
-    // The save is finished and written to disk safely
-    });
-
+var http = require("http")
+console.log("xxxxxxxxx")
+http.get("http://127.0.0.1:5984/", function(res) {
+  console.log("Got response: " + res.statusCode);
+  console.log(res)
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
 });
-
-
