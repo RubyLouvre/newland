df1ae92b0d9a1a8075044bb635a846ceb4bc694a
RubyLouvre <cheng19840218@gmail.com>
2012-09-01 11:09:24 +0800

    update!

--- system/services/respond_to.js
+++ system/services/respond_to.js
@@ -21,7 +21,12 @@ define( ["$ejs"], function(){
             if( flow.mime == "*" ){//如果是页面
                 var cache = $.pagesCache[ url ];
                 var array = this.helper;
-                var context = array[0];
+             //   var context = array[0];
+                var context = {
+                    links:[],
+                    scripts:[],
+                    sign: true
+                }
                 var helpers = array[1];
                 var temp, html //用于保存erb或html
                 if(!cache){//如果不存在,先尝试打模板
@@ -37,13 +42,23 @@ define( ["$ejs"], function(){
                 if(!cache){//如果再不存在则找静态页面
                     cache = getFile( url, "html" );
                 }
-                if(cache && cache.type !== "html"){//如果找到的是模板
+                if(!cache){//如果还是没有找到404
+                    res.setHeader('Content-Type', "text/plain");
+                    res.setHeader('Content-Length', Buffer.byteLength("404"));
+                    res.end("404");
+                    return
+                }else if(typeof cache.data == "function"){
+                   //   console.log(context)
+                        console.log(context)
                     html = cache.data( context, helpers );//转换成页面
+                    console.log("context.layout "+context.layout);
+                    console.log(context)
                     if(typeof context.layout == "string"){//如果它还要依赖布局模板才能成为一个完整页面,则找布局模板去
                         context.partial = html;
                         var layout_url = $.path.join("app","views/layout", context.layout );
                         var fn = $.pagesCache[ layout_url ]
-                        if(!fn){
+                        //    console.log(fn)
+                        if( ! fn ){
                             try{
                                 temp = $.readFileSync(layout_url,"utf8");
                                 fn = $.ejs.compile( temp, helpers );//转换成编译函数
@@ -55,17 +70,19 @@ define( ["$ejs"], function(){
                         }
                         html = fn( context, helpers );//这时已是完整页面了
                     }
+                    //这时应该调用sendFile
                     cache = {
                         data: html,
                         type: "html"
                     }
-                //这时应该调用sendFile
                 }
             }else{
                 cache = $.pagesCache[ url ];
                 if(!cache){
                     cache = getFile( url, flow.mime );
                 }
+                console.log("==================");
+                console.log( cache.type );
             }
             var data = cache.data;//要返回给前端的数据
             if(data.type  === "json"){
