6e2cf1b3759234c40dcb21abe29837696cb7716e
RubyLouvre <cheng19840218@gmail.com>
2012-09-02 20:45:46 +0800

    添加有用内容

--- system/services/timeout.js
+++ system/services/timeout.js
@@ -9,3 +9,57 @@ define( function(){
         });
     }
 })
+
+//计算字节因该用这个代码
+
+//// 字节长度
+//// utf-16 是 javascript 本地字符集
+//string.sizeof = function (str, charset)
+//{
+//		charset = (charset || 'UTF8').toUpperCase();
+//
+//		var i = 0, j = str.length, n = 0, c;
+//
+//		if ('UTF16' === charset)
+//		{
+//				for (; i < j; i++)
+//				{
+//						c = str.charCodeAt(i);
+//
+//						if (c < 65536)
+//						{
+//								n += 2;
+//						}
+//						else
+//						{
+//								n += 4;
+//						}
+//				}
+//		}
+//		else if ('UTF8' === charset)
+//		{
+//				for (; i < j; i++)
+//				{
+//						c = str.charCodeAt(i);
+//
+//						if (c < 128)
+//						{
+//								n++;
+//						}
+//						else if (c < 2048)
+//						{
+//								n += 2;
+//						}
+//						else if (c < 65536)
+//						{
+//								n += 3;
+//						}
+//						else
+//						{
+//								n += 4;
+//						}
+//				}
+//		}
+//
+//		return n;
+//};
