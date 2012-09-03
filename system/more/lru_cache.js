56bc2d97960bd92e6c17d472631db6408fa004f7
司徒正美 <cheng19840218@gmail.com>
2012-09-03 00:00:49 -0700

    更新LRU！

--- system/more/lru_cache.js
+++ system/more/lru_cache.js
@@ -1,197 +1,208 @@
-//http://cnodejs.org/topic/50210100f767cc9a51de4f3a
-//author: Sun, Junyi (weibo.com/treapdb)
-//usage: node --nouse-idle-notification --expose-gc --max-old-space-size=8192 memcached.js
+//https://github.com/isaacs/node-lru-cache
 
-var config ={
-  port: 11211,
-  max_memory: 100 // default 100M bytes
-}
+define("lru_cache", function(){
+    function naiveLength () {
+        return 1
+    }
 
-var net = require('net');
-var LRU = function (max) { // this LRU implementaion is based on https://github.com/chriso/lru
-    this.cache = {}
-    this.head = this.tail = null;
-    this.length = 0;
-    this.max = max || config.max_memory << 20;
-    setInterval(function(lru_store){
-        evict_count = parseInt(lru_store.length/3)
-        for(i=0;i<evict_count;i++){
-          if(process.memoryUsage().rss > lru_store.max)
-            lru_store.evict()
-          else
-            break;
+   return function LRUCache (options) {
+        if (!(this instanceof LRUCache)) {
+            return new LRUCache(options)
         }
-        console.log("element count:", lru_store.length)
 
-    },2000,this)  // detection of LRU eviction every 2 seconds
-};
-
-LRU.prototype.remove = function (key) {
-  var element = this.cache[key];
-  if(element) {
-    delete this.cache[key];
-    this.length --;
-    if(element.prev) this.cache[element.prev].next = element.next;
-    if(element.next) this.cache[element.next].prev = element.prev;
-    if(this.head == key) {
-      this.head = element.prev;
-    }
-    if(this.tail == key) {
-      this.tail = element.next;
-    }
-  }
-  return element;
-}
-
-LRU.prototype.set = function (key, value) {
-    element = this.remove(key);
-    element = element || { value:value };
-
-    element.next = null;
-    element.prev = this.head;
-    element.value = value;
-
-    this.cache[key] = element;
-
-    if (this.head) {
-        this.cache[this.head].next = key;
-    }
-    this.head = key;
-
-    if(!this.tail) {
-      this.tail = key;
-    }
-    this.length ++
-    if(process.memoryUsage().rss > this.max){
-        this.evict()
-    }
-};
-
-LRU.prototype.get = function (key) {
-    var element = this.cache[key];
-    if (!element) { return; }
-
-    this.set(key, element.value);
-    return element.value;
-};
-
-LRU.prototype.evict = function () {
-    if(!this.tail) { return; }
-    var key = this.tail;
-    var element = this.remove(this.tail);
-};
-
-var store = new LRU()
-
-
-function handle_header(header,crlf_len){
-    var tup = header.split(" ")
-    var expect_body_len = 0
-    switch(tup[0]){
-        case 'get':
-        case 'delete':
-            expect_body_len = 0
-            break
-        case 'set':
-            expect_body_len = parseInt(tup[4]) + crlf_len
-            break
-        case 'gc': // this command is used for maintain
-            expect_body_len = 0
-            gc()
-            break;
-    }
-    return expect_body_len
-}
-
-function handle_body(socket,header,body,call_back){
-    var response=""
-    var tup = header.split(" ")
-    switch(tup[0]){
-        case 'get':
-            var key = tup[1]
-            var obj = store.get(key)
-            if(obj){
-                response = "VALUE "+ key+" " + obj.flag+" "  + obj.data.length + "\r\n"
-                response += obj.data + "\r\n"
-                response += "END\r\n"
+        var max
+        if (typeof options === 'number') {
+            max = options
+            options = {
+                max: max
             }
-            else
-                response = "NOT_FOUND\r\n"
-            break;
-        case 'delete':
-            var key = tup[1]
-            store.remove(key)
-            response = "DELETED\r\n"
-            break;
-        case 'set':
-            var key = tup[1]
-            var obj = {flag: tup[2], expire:0 , data: body}
-            store.set( key , obj)
-            response = "STORED\r\n"
-            break;
-        case 'gc': // this command is used for maintain
-            response = "OK\r\n"
-            break;
-        default:
-            response = "ERROR\r\n"
-            break;
+        }
+        max = options.max
+
+        if (!options) options = {}
+
+        var lengthCalculator = options.length || naiveLength
+
+        if (typeof lengthCalculator !== "function") {
+            lengthCalculator = naiveLength
+        }
+        if (!max || !(typeof max === "number") || max <= 0 ) {
+            max = Infinity
+        }
+
+        var maxAge = options.maxAge || null
+
+        var dispose = options.dispose
+
+        var cache = {} // hash of items by key
+        , lruList = {} // list of items in order of use recency
+        , lru = 0 // least recently used
+        , mru = 0 // most recently used
+        , length = 0 // number of items in the list
+        , itemCount = 0
+
+
+        // resize the cache when the max changes.
+        Object.defineProperty(this, "max",
+        {
+            set : function (mL) {
+                if (!mL || !(typeof mL === "number") || mL <= 0 ) mL = Infinity
+                max = mL
+                // if it gets above double max, trim right away.
+                // otherwise, do it whenever it's convenient.
+                if (length > max) trim()
+            }
+            ,
+            get : function () {
+                return max
+            }
+            ,
+            enumerable : true
+        })
+
+        // resize the cache when the lengthCalculator changes.
+        Object.defineProperty(this, "lengthCalculator",
+        {
+            set : function (lC) {
+                if (typeof lC !== "function") {
+                    lengthCalculator = naiveLength
+                    length = itemCount
+                    Object.keys(cache).forEach(function (key) {
+                        cache[key].length = 1
+                    })
+                } else {
+                    lengthCalculator = lC
+                    length = 0
+                    Object.keys(cache).forEach(function (key) {
+                        cache[key].length = lengthCalculator(cache[key].value)
+                        length += cache[key].length
+                    })
+                }
+
+                if (length > max) trim()
+            }
+            ,
+            get : function () {
+                return lengthCalculator
+            }
+            ,
+            enumerable : true
+        })
+
+        Object.defineProperty(this, "length",
+        {
+            get : function () {
+                return length
+            }
+            ,
+            enumerable : true
+        })
+
+
+        Object.defineProperty(this, "itemCount",
+        {
+            get : function () {
+                return itemCount
+            }
+            ,
+            enumerable : true
+        })
+
+        this.reset = function () {
+            if (dispose) {
+                Object.keys(cache).forEach(function (k) {
+                    dispose(k, cache[k].value)
+                })
+            }
+            cache = {}
+            lruList = {}
+            lru = 0
+            mru = 0
+            length = 0
+            itemCount = 0
+        }
+
+        // Provided for debugging/dev purposes only. No promises whatsoever that
+        // this API stays stable.
+        this.dump = function () {
+            return cache
+        }
+
+        this.set = function (key, value) {
+            if (hOP(cache, key)) {
+                // dispose of the old one before overwriting
+                if (dispose) dispose(key, cache[key].value)
+                if (maxAge) cache[key].now = Date.now()
+                cache[key].value = value
+                this.get(key)
+                return true
+            }
+
+            var hit = {
+                key:key,
+                value:value,
+                lu:mru++,
+                length:lengthCalculator(value),
+                now: (maxAge) ? Date.now() : 0
+            }
+
+            // oversized objects fall out of cache automatically.
+            if (hit.length > max) return false
+
+            length += hit.length
+            lruList[hit.lu] = cache[key] = hit
+            itemCount ++
+
+            if (length > max) trim()
+            return true
+        }
+
+        this.get = function (key) {
+            if (!hOP(cache, key)) return
+            var hit = cache[key]
+            if (maxAge && (Date.now() - hit.now > maxAge)) {
+                this.del(key)
+                return
+            }
+            delete lruList[hit.lu]
+            if (hit.lu === lru) lruWalk()
+            hit.lu = mru ++
+            lruList[hit.lu] = hit
+            return hit.value
+        }
+
+        this.del = function (key) {
+            if (!hOP(cache, key)) return
+            var hit = cache[key]
+            if (dispose) dispose(key, hit.value)
+            delete cache[key]
+            delete lruList[hit.lu]
+            if (hit.lu === lru) lruWalk()
+            length -= hit.length
+            itemCount --
+        }
+
+        function lruWalk () {
+            // lru has been deleted, hop up to the next hit.
+            for (var key in lruList) {
+                return key
+            }
+        }
+
+        function trim () {
+            if (length <= max) return
+            var prune = Object.keys(lruList)
+            for (var i = 0; i < prune.length && length > max; i ++) {
+                var hit = lruList[prune[i]]
+                if (dispose) dispose(hit.key, hit.value)
+                length -= hit.length
+                delete cache[ hit.key ]
+                delete lruList[prune[i]]
+            }
+
+            lruWalk()
+        }
     }
-    socket.write(response,"binary",call_back)
-}
 
-var server = net.createServer(function (socket) {
-    console.log("client: ",socket.remoteAddress)
-    var user_state = 'reading_header'
-    var buf = ""
-    var header =""
-    var body = ""
-    var expect_body_len = 0
-    var CRLF_LEN = 2
-    socket.setEncoding("binary")
-     socket.on('data',function(data){
-         buf += data
-         socket.emit('user_event') // we may got some data to handle
-     })
-     socket.on('user_event',function(){
-         switch(user_state){
-             case "reading_header": //if we are reading header
-                 var pos =-1
-                if((pos=buf.indexOf('\r\n'))!=-1){
-                    header = buf.slice(0,pos)
-                    buf = buf.slice(pos+2)
-                    CRLF_LEN =2
-                }
-                else if((pos=buf.indexOf('\n'))!=-1){
-                    header = buf.slice(0,pos)
-                    buf = buf.slice(pos+1)
-                    CRLF_LEN =1
-                }
-                if(pos!=-1){
-                    user_state = 'reading_body'
-                    expect_body_len = handle_header(header,CRLF_LEN)
-                    socket.emit("user_event")
-                }
-                 break
-             case "reading_body":  // if we are reading body
-                 if(expect_body_len <= buf.length){
-                     body = buf.slice(0,expect_body_len-CRLF_LEN)
-                     buf = buf.slice(expect_body_len)
-                     user_state = 'reading_header'
-                     handle_body(socket,header,body,
-                         function(){
-                             if(buf.length>0)
-                                 socket.emit("user_event")
-                         }
-                     )
+})
 
-                 }
-                 break
-         }
-     })
-});
-
-console.log("listening at "+ config.port)
-console.log("max memory: " + config.max_memory + "M bytes")
-server.listen(config.port, '0.0.0.0')
-
-setInterval(function(){gc(); console.log(process.memoryUsage());},5000) // manually garbage collection every 5 seconds
